28ded04ffd68e7ca784712282359d0c9c99959ef
GoopyGoblin <owen.c.singh@gmail.com>
2026-07-15 15:26:10 -0500

    Blocks

--- src/main/java/net/goblin/tutorialmod/TutorialMod.java
+++ src/main/java/net/goblin/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.goblin.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.goblin.tutorialmod.block.ModBlocks;
 import net.goblin.tutorialmod.item.ModCreativeModTabs;
 import net.goblin.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
@@ -31,6 +32,7 @@ public class TutorialMod
         ModCreativeModTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
--- src/main/java/net/goblin/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/goblin/tutorialmod/item/ModCreativeModTabs.java
@@ -1,6 +1,7 @@
 package net.goblin.tutorialmod.item;
 
 import net.goblin.tutorialmod.TutorialMod;
+import net.goblin.tutorialmod.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
@@ -20,6 +21,9 @@ public class ModCreativeModTabs {
                     {
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                     }))
                     .build());
 
