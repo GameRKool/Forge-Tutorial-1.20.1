524df36587155bc08d8f57f2b0e0fe06202492db
GoopyGoblin <owen.c.singh@gmail.com>
2026-07-15 14:37:20 -0500

    Items

--- src/main/java/net/goblin/tutorialmod/TutorialMod.java
+++ src/main/java/net/goblin/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.goblin.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.goblin.tutorialmod.item.ModCreativeModTabs;
+import net.goblin.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -24,6 +28,10 @@ public class TutorialMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -42,6 +50,10 @@ public class TutorialMod
 
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     @SubscribeEvent
